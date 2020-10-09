974bbf2146820a935c2773c3d5da212463fa26a7
Kenneth <gilmorekenneth@gmail.com>
2020-10-08 21:22:44 -0400

    Updates

---
+++ precache-manifest.acc87be3c250d7adbdbd3bc559dc1e2e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40e6194a756297ee6ae5a1d09a02b79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79da8e07139fb2d81a97",
+    "url": "/static/css/main.f8805a76.chunk.css"
+  },
+  {
+    "revision": "9b41bdb2e4f6ebed8fe4",
+    "url": "/static/js/2.c6ef9cf6.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.c6ef9cf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79da8e07139fb2d81a97",
+    "url": "/static/js/main.f4392cec.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "d1ed303c845229934bc5998a2bbde356",
+    "url": "/static/media/1.d1ed303c.png"
+  },
+  {
+    "revision": "b3802bef86f92d4ff5fa9e613745375e",
+    "url": "/static/media/4-cast-screenshot.b3802bef.png"
+  },
+  {
+    "revision": "e5b9b2a4c91f8eb4e0b9683a89c16d73",
+    "url": "/static/media/cartoon-coder.e5b9b2a4.jpg"
+  },
+  {
+    "revision": "3592a8335512c110f3e5eb113605ad52",
+    "url": "/static/media/code-on-laptop.3592a833.jpg"
+  },
+  {
+    "revision": "05702241b50be1b89758698d01d86522",
+    "url": "/static/media/mastermind-screenshot.05702241.png"
+  },
+  {
+    "revision": "dd7e05d28cc4e2a1578087b8cdbc1820",
+    "url": "/static/media/plantarama-screenshot.dd7e05d2.png"
+  },
+  {
+    "revision": "ca1aebc693487d0dc2f77244f5d8d303",
+    "url": "/static/media/scott-webb-ffWnaif_9T0-unsplash.ca1aebc6.jpg"
+  },
+  {
+    "revision": "c19a77949c99436187ba10366351e95d",
+    "url": "/static/media/talkin-trash-screenshot.c19a7794.png"
+  }
+]);
